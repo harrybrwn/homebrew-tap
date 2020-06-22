b1dda7ace72ab070dc65e6cca86d7fe12cb8ea8a
yt-releasebot <harrybrown98@gmail.com>
2020-06-22 13:00:53 -0700

    Brew formula update for yt version v0.1.0

--- Formula/yt.rb
+++ Formula/yt.rb
@@ -2,21 +2,21 @@
 class Yt < Formula
   desc "Command line YouTube video and audio downloader"
   homepage "https://github.com/harrybrwn/yt"
-  version "0.0.3"
+  version "0.1.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/harrybrwn/yt/releases/download/v0.0.3/yt_0.0.3_MacOS_x86_64.tar.gz"
-    sha256 "874dc01d53212ca4fe82e96959b5bcffa0c96003dbee83905d93934545ffc2ba"
+    url "https://github.com/harrybrwn/yt/releases/download/v0.1.0/yt_0.1.0_MacOS_x86_64.tar.gz"
+    sha256 "56d9eeba445ad4fdfb6863f951f1af68cc81630ffaa055d5f0142d68b32eaba3"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/harrybrwn/yt/releases/download/v0.0.3/yt_0.0.3_Linux_x86_64.tar.gz"
-      sha256 "5fe5f34ce25a569644ff6e6ef1eecd2bf845f47662b76108a94bb709a414c283"
+      url "https://github.com/harrybrwn/yt/releases/download/v0.1.0/yt_0.1.0_Linux_x86_64.tar.gz"
+      sha256 "0f56fb3b2384a740e9017f48b2a14687e36be6c3aa627413a9a13863afcf25b1"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/harrybrwn/yt/releases/download/v0.0.3/yt_0.0.3_Linux_arm64.tar.gz"
-        sha256 "a538c0450dc40c805fb6ad6d79d1a88912e1ba207cac95ece01ae374a69c7ccf"
+        url "https://github.com/harrybrwn/yt/releases/download/v0.1.0/yt_0.1.0_Linux_arm64.tar.gz"
+        sha256 "0f86206e49a2ced5da2972f1d9f26d1cc6aaf774d3c12e103e667a9aa2e091a8"
       else
       end
     end
